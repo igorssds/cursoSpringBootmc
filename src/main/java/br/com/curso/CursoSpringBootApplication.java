f8bac62dc0cd8056af97fae5c13915a038971420
Igor Sampaio <igorsampaiosena@hotmail.com>
2018-08-20 11:58:00 -0300

    Projeto Criado

---
+++ src/main/java/br/com/curso/CursoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package br.com.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootApplication.class, args);
+	}
+}
