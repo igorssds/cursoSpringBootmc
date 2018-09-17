fa9ef95911e5ab8a5bc52650ece748ac1c65e563
igorssds <igorsampaiosena@hotmail.com>
2018-09-17 15:23:47 -0300

    Criando profile de teste

---
+++ src/main/java/br/com/curso/config/TestConfig.java
@@ -0,0 +1,24 @@
+package br.com.curso.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.curso.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbservice;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbservice.instantiateTestDatabase();
+		return true;
+	}
+}
